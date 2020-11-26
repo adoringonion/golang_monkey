a77e49501a400e858e39dca9b374c79be9369052
Fumihito Morita <adringonion.byazura@gmail.com>
2020-11-27 00:56:38 +0900

    114まで

--- parser/parser.go
+++ parser/parser.go
@@ -134,8 +134,11 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		return nil
 	}
 
-	// TODO: セミコロンに遭遇するまで式を読み飛ばしてしまっている
-	for !p.curTokenIs(token.SEMICOLON) {
+	p.nextToken()
+
+	stmt.Value = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
@@ -147,7 +150,9 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 	p.nextToken()
 
-	for !p.curTokenIs(token.SEMICOLON) {
+	stmt.ReturnValue = p.parseExpression(LOWEST)
+
+	for p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
--- repl/repl.go
+++ repl/repl.go
@@ -4,8 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"github.com/adoringonion/golang_monkey/token"
 	"github.com/adoringonion/golang_monkey/lexer"
+	"github.com/adoringonion/golang_monkey/parser"
 )
 
 const PROMPT = ">> "
@@ -22,9 +22,34 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 	}
 }
+
+
+const HATTO_SHIN = `
+　          ＿＿＿_
+　 　　　／⌒　　   ⌒ ＼
+　　　／  （●） 　（●）  ＼
+　 ／  ::⌒ (__人__) ⌒::     ＼ 
+　 |　　　　　|r┬-|　　　　　|
+　 ＼ 　　 　  ー' 　 　   ／
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, HATTO_SHIN)
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
+
