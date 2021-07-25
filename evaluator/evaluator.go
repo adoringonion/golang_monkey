4ac085a0af6ad4e32d07b254bd49b486bf31a245
adoringonion <adringonion.byazura@gmail.com>
2021-07-26 01:11:19 +0900

    wip

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -14,7 +14,10 @@ var (
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
-		return evalStatements(node.Statements)
+		return evalProgram(node)
+
+	case *ast.BlockStatement:
+		return evalBlockStatement(node)
 
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
@@ -34,8 +37,6 @@ func Eval(node ast.Node) object.Object {
 		right := Eval(node.Right)
 		return evalInfixExpression(node.Operator, left, right)
 
-	case *ast.BlockStatement:
-		return evalStatements(node.Statements)
 
 	case *ast.IfExpression:
 		return evalIfExpression(node)
@@ -48,10 +49,10 @@ func Eval(node ast.Node) object.Object {
 	return nil
 }
 
-func evalStatements(stmts []ast.Statement) object.Object {
+func evalProgram(program *ast.Program) object.Object {
 	var result object.Object
 
-	for _, statement := range stmts {
+	for _, statement := range program.Statements {
 		result = Eval(statement)
 
 		if returnValue, ok := result.(*object.ReturnValue); ok {
@@ -59,6 +60,20 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		}
 	}
 
+	return result
+}
+
+func evalBlockStatement(block *ast.BlockStatement) object.Object {
+	var result object.Object
+
+	for _, statement := range block.Statements {
+		result := Eval(statement)
+
+		if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
+			return result
+		}
+	}
+	
 	return result
 }
 
