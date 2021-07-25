bd078e2082a23e5220085d4d88e4976caf5420dc
adoringonion <adringonion.byazura@gmail.com>
2021-07-25 20:38:57 +0900

    p131

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -1,6 +1,6 @@
 package evaluator
 
-import(
+import (
 	"github.com/adoringonion/golang_monkey/lexer"
 	"github.com/adoringonion/golang_monkey/object"
 	"github.com/adoringonion/golang_monkey/parser"
@@ -9,9 +9,9 @@ import(
 
 func TestEvalIntegerExpression(t *testing.T) {
 	tests := []struct {
-		input string
+		input    string
 		expected int64
-	} {
+	}{
 		{"5", 5},
 		{"10", 10},
 	}
@@ -38,6 +38,35 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 	}
 	if result.Value != expected {
 		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+		return false
+	}
+
+	return true
+}
+
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%t, want=%t", result.Value, expected)
 		return false
 	}
 
