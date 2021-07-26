abefe1ef212562cd70d048f80cf9c67663a80fe4
adoringonion <adringonion.byazura@gmail.com>
2021-07-26 23:44:46 +0900

    p160

--- repl/repl.go
+++ repl/repl.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/adoringonion/golang_monkey/evaluator"
 	"github.com/adoringonion/golang_monkey/lexer"
+	"github.com/adoringonion/golang_monkey/object"
 	"github.com/adoringonion/golang_monkey/parser"
 )
 
@@ -15,6 +16,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Print(PROMPT)
@@ -37,7 +39,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
