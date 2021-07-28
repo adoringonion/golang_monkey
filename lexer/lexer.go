bbca055957ce32cc3eb8aa78c59c1b4e49a2d427
adoringonion <adringonion.byazura@gmail.com>
2021-07-29 02:12:44 +0900

    p182

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -76,6 +76,9 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	case '"':
+		tok.Type = token.STRING
+		tok.Literal = l.readString()
 	default:
 		if isLetter(l.ch) {
 			tok.Literal = l.readIdentifier()
@@ -154,4 +157,15 @@ func LookupIdent(ident string) token.TokenType {
 	}
 
 	return token.IDENT
+}
+
+func (l *Lexer) readString() string {
+	position := l.position + 1
+	for {
+		l.readChar()
+		if l.ch == '"' || l.ch == 0 {
+			break
+		}
+	}
+	return l.input[position:l.position]
 }
--- token/token.go
+++ token/token.go
@@ -42,4 +42,6 @@ const (
 
 	EQ     = "=="
 	NOT_EQ = "!="
+
+	STRING = "STRING"
 )
